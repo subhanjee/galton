6f44e4f75cd20472442537655022133b94208349
subhanje <subhanjee14@gmail.com>
2024-01-12 17:59:39 +0500

    doneapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 // project import
-// import Routes from 'routes';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/ScrollTop';
-// import MainRoutes from 'routes/MainRoutes';
 import ThemeRoutes from 'routes/index';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
@@ -10,7 +8,6 @@ import ThemeRoutes from 'routes/index';
 const App = () => (
   <ThemeCustomization>
     <ScrollTop>
-      {/* <Routes /> */}
       <ThemeRoutes />
     </ScrollTop>
   </ThemeCustomization>
