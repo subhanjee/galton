6be5efac72e017c6b525c71a68ba807c5257d4b0
develp-Abdullah <devrlpor.abdullah@gmail.ccom>
2024-01-17 01:01:28 +0500

    idk

--- src/pages/extra-pages/Ask.js
+++ src/pages/extra-pages/Ask.js
@@ -24,140 +24,67 @@ const Ask = () => {
   const [loadingMessage, setLoadingMessage] = useState('');
   const [currentStep, setCurrentStep] = useState(0);
 
-  // Inside the component function
-  // Function to call the API
-  // const callApi = async () => {
-  //   try {
-  //     setLoading(true);
-  //     const createThreadResponse = await fetch('http://127.0.0.1:8000/create_thread', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify({ query: queryString }) // Replace 'text' with the actual parameter name
-  //     });
-
-  //     const createThreadData = await createThreadResponse.json();
-  //     // setApiResponse(data);
-
-  //     const diggerCall = await fetch('http://127.0.0.1:8000/call_digger', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify({ manager_response: createThreadData['manager_response'] }) // Replace 'text' with the actual parameter name
-  //     });
-  //     setLoading(false);
-  //     const diggerData = await diggerCall.json();
-  //     setdiggerResponse(diggerData);
-
-  //     const chartifyCall = await fetch('http://127.0.0.1:8000/call_chartify', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify({ manager_response: createThreadData['manager_response'] }) // Replace 'text' with the actual parameter name
-  //     });
-
-  //     const chartifyData = await chartifyCall.json();
-  //     if (chartifyData.status == 'success' && 'chart' in chartifyData) {
-  //       var chartData = JSON.parse(chartifyData.chart);
-  //       chartifyData.chart = chartData;
-  //     }
-  //     setchartResponse(chartifyData);
-
-  //     const callInsighter = await fetch('http://127.0.0.1:8000/call_insightor', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify({ manager_response: createThreadData['manager_response'] }) // Replace 'text' with the actual parameter name
-  //     });
-
-  //     const insighterData = await callInsighter.json();
-  //     setinsightResponse(insighterData);
-  //   } catch (error) {
-  //     console.error('Error fetching API:', error);
-  //     // Handle error if needed
-  //   } finally {
-  //     setLoading(false); // Set loading to false when API call is completed (success or failure)
-  //     setShowApiSection(true); // Show the API response section
-  //   }
-  // };
   const Thread = async () => {
-    try {
-      // First, make the createThread API call
-      setLoading(true);
-      // Step 1: Display "Please wait, creating thread..."
-      setLoadingMessage('Please wait, creating thread...');
-      const first = await createThread({
-        query: queryString
-      });
+    setLoading(true);
+    setLoadingMessage('Manager Agent Analysing the query');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Manager agent figuring out the quests for the team');
+    const first = await createThread({ query: queryString });
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Manager Agent communicating with the team');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(first);
+    callDig(first);
+  };
 
-      console.log(first, 'thread');
+  const callDig = async (createThread) => {
+    setLoadingMessage('Analyser Agent received the query. ');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Analyser Agent listing down required parameters');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Analyser Agent using python to query the data');
+    const secound = await createCallDigger({ manager_response: createThread.manager_response });
+    setCurrentStep(1);
+    console.log(secound.response);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Analyser Agent pulling out relevant details');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setCallDigger(secound.response);
+    callChartify(createThread);
+  };
 
-      // Now, make the createCallDigger API call
-      // Step 2: Display "Please wait, calling Digger..."
-      setCurrentStep(1);
+  const callChartify = async (createThread) => {
+    setLoadingMessage('Visualiser Agent received the query.');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Visualiser Agent listing down required parameters');
+    const charityResponse = await createCallChartify({ manager_response: createThread.manager_response });
+    const cleanedData = charityResponse.chart.replace(/^```json\s+|\s+```$/g, '');
+    console.log(cleanedData);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Visualiser Agent using relevant details and creating a chart');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setCallcharity(cleanedData);
+    setCurrentStep(2);
+    callInsight(createThread);
+  };
 
-      setLoadingMessage('Please wait, calling Digger...');
-      const calldraggerResponse = await createCallDigger({
-        manager_response: first?.manager_response
-      });
-      setCallDigger(calldraggerResponse.response);
-      console.log(calldraggerResponse, 'calldraggerResponse');
-      // Add the new API call - callcharity
-      // Step 3: Display "Please wait, calling Chartify..."
-      setCurrentStep(2);
-
-      setLoadingMessage('Please wait, calling Chartify...');
-      const charityResponse = await createCallChartify({
-        manager_response: first?.manager_response
-      });
-      console.log(charityResponse, 'charityResponse');
-      const cleanedData = charityResponse.chart.replace(/^```json\s+|\s+```$/g, '');
-      console.log('Cleaned Data:', cleanedData);
-      setCallcharity(cleanedData);
-
-      // Add the new API call - callinsightor
-      // Step 4: Display "Please wait, calling Insightor..."
-      setCurrentStep(3);
-
-      setLoadingMessage('Please wait, calling Insightor...');
-      const insightorResponse = await createCallInsightor({
-        manager_response: first?.manager_response
-      });
-      setCallInsightor(insightorResponse.insight);
-      console.log(insightorResponse, 'insightorResponse');
-
-      // Step 5: Display "Loading completed. Data is ready."
-      setCurrentStep(4);
-
-      setLoadingMessage('Loading completed. Data is ready.');
-      // Code to execute after completion of all API calls
-      setLoading(false);
-
-      // Invoke the checkStatusUntilCompleted function
-    } catch (error) {
-      // Handle errors as needed
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.error('Error response data:', error.response.data);
-        console.error('Error response status:', error.response.status);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.error('Error making the request:', error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.error('Error during request setup:', error.message);
-      }
-      setLoading(false);
-    }
+  const callInsight = async (createThread) => {
+    setLoadingMessage('Insight Agent Figuring out a relevant insight');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Insight Agent using python to query the data');
+    const insightorResponse = await createCallInsightor({ manager_response: createThread.manager_response });
+    setCurrentStep(3);
+    console.log(insightorResponse);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setLoadingMessage('Insight Agent pulling out relevant details and adding comment');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setCallInsightor(insightorResponse.insight);
+    setLoading(false);
   };
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       Thread();
+      setLoading(true);
     }
   };
   return (
